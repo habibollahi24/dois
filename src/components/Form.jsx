9a6e3bd3976bf15651b31e08e6a96f6aff76aa27
mohammad <habibollahi24@gmail.com>
2023-05-01 13:45:18 +0330

    add padding

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,17 @@
+/* eslint-disable react/prop-types */
 const Form = ({ submitHandler, consortiumId, setCosortiumId }) => {
    return (
       <form
          onSubmit={submitHandler}
-         className="flex justify-center space-x-2 items-center"
+         className="flex justify-center space-x-2 items-center p-1"
       >
-         <label className="text-xs text-gray-600 font-semibold">consortium id :</label>
+         <label className="text-xs text-gray-600 font-semibold">cons_id:</label>
          <input
             placeholder="Example daraco ..."
             className="border-2 rounded-md px-3 py-1  focus:outline-none placeholder:text-xs"
             type="text"
             value={consortiumId}
-            onChange={(e) => setCosortiumId(e.target.value)}
+            onChange={(e) => setCosortiumId(e.target.value.toLowerCase())}
          />
          <button
             className="border-2 rounded-md px-2 py-1 bg-gray-200 text-gray-600 font-semibold"
